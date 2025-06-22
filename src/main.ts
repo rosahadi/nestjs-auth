fd0e8d76556d501a033ec7d87380e0aa3be4a64a
rosa <rosahadi19@gmail.com>
2025-06-22 17:28:21 +0300

    chore: initialize NestJS and configure global pipes and cookie parser

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  // Essential for validating DTOs
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  // Enable reading cookies
+  app.use(cookieParser());
+
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
